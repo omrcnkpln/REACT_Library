c7c7f0fc9f79a688a868c243b7a12198e83979b9
ok0097 <omer.kaplan@fups.com>
2023-04-20 14:38:32 +0300

    ant-design-upload

---
+++ antDesign.Upload.Base64.js
@@ -0,0 +1,32 @@
+//jsx
+<Upload
+    ref={this.myRef}
+    accept="*"
+    {...this.antProps}
+>
+    <Button>Dosya Yükle</Button>
+</Upload>
+
+// prop
+antProps: UploadProps = {
+    beforeUpload: this.beforeUpload,
+    multiple: false,
+}
+
+//method
+beforeUpload: UploadProps['beforeUpload'] = (file) => {
+    const reader = new FileReader();
+    reader.readAsDataURL(file);
+
+    reader.onload = (event) => {
+        this.setState({
+            attachment: reader.result
+        })
+    };
+
+    this.setState({
+        filename: file.name,
+    })
+
+    return false;
+}
